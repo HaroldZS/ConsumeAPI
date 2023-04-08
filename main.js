21940ae759cf7ffad07608a78a207fa8bfb9eef7
haroldZS <haroldzs1024@gmail.com>
2023-04-08 16:23:12 -0400

    Random cats interface

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+const URL = "https://api.thecatapi.com/v1/images/search";
+
+fetch(URL)
+  .then((res) => res.json())
+  .then((data) => {
+    const img = document.querySelector("img");
+    img.src = data[0].url;
+  });
+
+// async function getImage() {
+//   try {
+//     const response = await fetch(URL);
+//     const data = await response.json();
+//     const img = document.querySelector("img");
+//     img.src = data[0].url;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// getImage();
+
+function randomCat() {
+  location.reload();
+}
