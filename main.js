b52bb8caa8cde630a70b9cfe59d3eefda7c3e748
haroldZS <haroldzs1024@gmail.com>
2023-04-08 17:04:24 -0400

    Queryselectors

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-const URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
 
-fetch(URL)
-  .then((res) => res.json())
-  .then((data) => {
-    const img = document.querySelector("img");
-    img.src = data[0].url;
-  });
+async function getImage() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const img3 = document.getElementById("img3");
 
-// async function getImage() {
-//   try {
-//     const response = await fetch(URL);
-//     const data = await response.json();
-//     const img = document.querySelector("img");
-//     img.src = data[0].url;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getImage();
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getImage();
 
 function randomCat() {
   location.reload();
