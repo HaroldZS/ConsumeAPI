b198aa072041c90f6cf9c3b24dc9b0d98b6dae32
haroldZS <haroldzs1024@gmail.com>
2023-04-08 18:26:54 -0400

    Interface Layout

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_3HtJzwaOwPebbHgkty8tBfHw7SUqRqOjtoOciTWhWTmWRP0C6sspe1c7XbFwewdw";
 
 async function getImage() {
   try {
@@ -8,6 +8,7 @@ async function getImage() {
     const img2 = document.getElementById("img2");
     const img3 = document.getElementById("img3");
 
+    console.log(data);
     img1.src = data[0].url;
     img2.src = data[1].url;
     img3.src = data[2].url;
